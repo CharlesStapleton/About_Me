01f89aaaedd70dfbbc5313c0b75dddd09bf4f79d
Charles Stapleton <stapletc@hotmail.com>
2018-09-13 19:05:07 -0700

    added second question

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ var userName = prompt('Hi, I\'m Charles! What\'s your name?');
 alert('Hey there ' + userName + '! I\'m gonna have you play a guessing game about me. Please answer with Y/N or Yes/No.');
 console.log('What is your name: ' + userName);
 
-// Doom Question
+// Doom 2016 Game of the Year Question
 var doom = prompt('Was Charles\' Game of the Year for 2016 Doom?');
 var doomUppercase = doom.toUpperCase();
 if (doomUppercase === 'Y' || doomUppercase === 'YES') {
@@ -15,4 +15,16 @@ if (doomUppercase === 'Y' || doomUppercase === 'YES') {
 } else {
   alert('Please answer with Y/N or Yes/No.');
 }
-console.log('Doom was Charles\' 2016 Game of the Year: ' + doom);
+console.log('Was Doom Charles\' 2016 Game of the Year? User guessed: ' + doom);
+
+// Favorite Sport Question
+var favSport = prompt('Is Basketball Charles\' favorite sport?');
+var favSportUppercase = favSport.toUpperCase();
+if (favSportUppercase === 'Y' || favSportUppercase === 'YES') {
+  alert('Correct! Ball is life.');
+} else if (favSportUppercase === 'N' || favSportUppercase === 'NO') {
+  alert('Wrong. Ball is life.');
+} else {
+  alert('Please answer with Y/N or Yes/No.');
+}
+console.log('Is Basketball Charles\' favorite sport? User guessed: ' + favSport);
