455823ee381fa64bfae527d69bd0c649a444f4e3
Charles Stapleton <stapletc@hotmail.com>
2018-09-15 13:18:59 -0700

    finished lab 3

--- js/app.js
+++ js/app.js
@@ -1,15 +1,17 @@
 'use strict';
+var numOfCorrectAnswers = 0;
 
-// // Intro / UserName Question
-// var userName = prompt('Hi, I\'m Charles! What\'s your name?');
-// alert('Hey there ' + userName + '! I\'m gonna have you play a guessing game about me. Please answer with Y/N or Yes/No.');
-// console.log('What is your name: ' + userName);
+// Intro / UserName Question
+var userName = prompt('Hi, I\'m Charles! What\'s your name?');
+alert('Hey there ' + userName + '! I\'m gonna have you play a guessing game about me. Please answer with Y/N or Yes/No.');
+console.log('What is your name: ' + userName);
 
 // // Doom 2016 Game of the Year Question
 // var doom = prompt('Was Charles\' Game of the Year for 2016 Doom?');
 // var doomUppercase = doom.toUpperCase();
 // if (doomUppercase === 'Y' || doomUppercase === 'YES') {
 //   alert('Correct! You\'ve got good taste.');
+//   numOfCorrectAnswers++;
 // } else if (doomUppercase === 'N' || doomUppercase === 'NO') {
 //   alert('Wrong. I can\'t believe you\'ve done this.');
 // } else {
@@ -22,6 +24,7 @@
 // var favSportUppercase = favSport.toUpperCase();
 // if (favSportUppercase === 'Y' || favSportUppercase === 'YES') {
 //   alert('Correct! Ball is life.');
+//   numOfCorrectAnswers++;
 // } else if (favSportUppercase === 'N' || favSportUppercase === 'NO') {
 //   alert('Wrong. Ball is life.');
 // } else {
@@ -36,6 +39,7 @@
 //   alert('Wrong! It\'s Orange. Green is a runner up tho.');
 // } else if (favColorUppercase === 'N' || favColorUppercase === 'NO') {
 //   alert('Correct! Orange is best.');
+//   numOfCorrectAnswers++;
 // } else {
 //   alert('Please answer with Y/N or Yes/No.');
 // }
@@ -46,6 +50,7 @@
 // var perfectUppercase = perfect.toUpperCase();
 // if (perfectUppercase === 'Y' || perfectUppercase === 'YES') {
 //   alert('Correct! I\'ve never done anything wrong.');
+//   numOfCorrectAnswers++;
 // } else if (perfectUppercase === 'N' || perfectUppercase === 'NO') {
 //   alert('Incorrect! I\'ve never done anything wrong.');
 // } else {
@@ -58,6 +63,7 @@
 // var catUppercase = cat.toUpperCase();
 // if (catUppercase === 'Y' || catUppercase === 'YES') {
 //   alert('Correct! Her name is Tux and she is also perfect.');
+//   numOfCorrectAnswers++;
 // } else if (catUppercase === 'N' || catUppercase === 'NO') {
 //   alert('Wrong! I own a cat, her name is Tux and she is also perfect.');
 // } else {
@@ -65,24 +71,26 @@
 // }
 // console.log('Does Charles own a cat? User guessed: ' + cat);
 
-// How Many Pets Question
-var numOfPets = '6';
-var guesses = 4;
+// // How Many Pets Question
+// var numOfPets = '6';
+// var guesses = 4;
 
-while (guesses > 0) {
-  var petsGuess = prompt('How many pets has Charles owned so far? You\'ve got 4 tries.');
-  if (petsGuess === numOfPets) {
-    alert('Correct! I\'ve owned 6 pets so far.');
-    break;
-  } if (petsGuess<'6') {
-    alert('Nope! Too low.');
-    guesses--;
-  } if (petsGuess>'6') {
-    alert('Nope! Too high.');
-    guesses--;
-  }
-}
+// while (guesses > 0) {
+//   var petsGuess = prompt('How many pets has Charles owned so far? You\'ve got 4 tries.');
+//   if (petsGuess === numOfPets) {
+//     alert('Correct! I\'ve owned 6 pets so far.');
+//     numOfCorrectAnswers++;
+//     break;
+//   } if (petsGuess<'6') {
+//     alert('Nope! Too low.');
+//     guesses--;
+//   } if (petsGuess>'6') {
+//     alert('Nope! Too high.');
+//     guesses--;
+//   }
+// }
 
+// DELETE LATER
 // for (var i=0; i<4; i++) {
 //   if (petsGuess === numOfPets) {
 //     alert('Correct! I\'ve owned 6 pets so far.');
@@ -100,3 +108,48 @@ while (guesses > 0) {
 //   //   prompt('Nope! Guess again.');
 //   // }
 // }
+
+// Countries I Want to Visit Question
+var countries = ['JAPAN', 'KOREA', 'CHINA', 'RUSSIA', 'FRANCE'];
+var correctAnswer = false;
+var numOfGuesses = 6;
+
+// while (numOfGuesses > 0) {
+//   var countryAnswer = prompt('What countries does Charles want to visit? You\'ve got 6 guesses.');
+//   if (countryAnswer === countries.length) {
+//     alert('Correct! I want to visit: Japan, Korea, China, Russia and France.');
+//     break;
+//   } if (countryAnswer<'6') {
+//     alert('Nope! Too low.');
+//     numOfGuesses--;
+//   } if (countryAnswer>'6') {
+//     alert('Nope! Too high.');
+//     numOfGuesses--;
+//   }
+// }
+
+do {
+  var countryAnswer = prompt('What countries does Charles want to visit? You\'ve got 6 guesses.');
+  var countryAnswerUppercase = countryAnswer.toUpperCase();
+  for (var i = 0; i < countries.length; i++) {
+    if (countryAnswerUppercase === countries[i]) {
+      correctAnswer = true;
+      numOfCorrectAnswers++;
+      break;
+    }
+  }
+  if (correctAnswer) {
+    // alert(countries[i]);
+    alert('Great job! I want to visit: Japan, Korea, China, Russia and France.');
+    numOfGuesses++;
+  } else {
+    alert('Incorrect, try again.');
+  }
+  numOfGuesses--;
+} while (numOfGuesses > 0 && !correctAnswer);
+
+if (!correctAnswer) {
+  alert('Out of guesses. I want to visit: Japan, Korea, China, Russia and France.');
+}
+
+alert('Thanks for playing my game! You got ' + numOfCorrectAnswers + ' out of 7 questions right, ' + userName + '.');
